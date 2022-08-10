d597940b5322db56d492e334a26ca01dfe34998d
Mazlum Kahraman <mazlummkahraman@gmail.com>
2022-08-10 21:44:54 +0300

    Singleton Added

---
+++ Singleton/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customerManager = CustomerManager.CreateAsSingleton();
+            customerManager.Save();
+        }
+    }
+
+    class CustomerManager
+    {
+        static CustomerManager _customerManager;
+        private CustomerManager()
+        {
+
+        }
+
+        public static CustomerManager CreateAsSingleton()
+        {
+            return _customerManager ?? (_customerManager = new CustomerManager());
+        }
+        public void Save()
+        {
+            Console.WriteLine("Saved!");
+        }
+    }
+}
